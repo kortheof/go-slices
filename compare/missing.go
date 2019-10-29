4027c2497d940947346e31f74200c5db544e25a3
Fanis Korlos <fanis.korlos@upstreamsystems.com>
2019-10-29 10:17:20 +0200

    Adding third/non-working solution

--- compare/missing.go
+++ compare/missing.go
@@ -3,24 +3,18 @@ package compare
 // FindMissingElement compares two slices of different length,
 // and returns the extra element between the two
 func FindMissingElement(s1, s2 []int) int {
-	// Find which slice is longer
-	var long, short []int
-	if len(s1) > len(s2) {
-		long, short = s1, s2
-	} else {
-		long, short = s2, s1
-	}
-	var missing int
-
-	for _, v1 := range long {
-		for _, v2 := range short {
-			if v1 == v2 {
-				// found = true
-				break
+	s := append(s1,s2...)
+	for len(s) > 1 {
+		// value := s[0]
+		for i, v := range s[:] {
+			if i == 0 {
+				continue
 			}
-			missing = v1
+			if s[0] == v { // Found duplicate
+				s[i], s[len(s)-1] = s[len(s)-1], s[i] // Put duplicate value in the end
+				s = s[:len(s)-1] // Re-slice to exlude the last element (duplicates))
+			}
 		}
 	}
-	return missing
-
-}
+	return s[0]
+	}
