a480befb50cf59aa5acbf9d5550eed69a57d243a
Fanis Korlos <fanis.korlos@upstreamsystems.com>
2019-11-04 12:44:54 +0200

    Fixing format with go linter

--- compare/missing.go
+++ compare/missing.go
@@ -3,10 +3,10 @@ package compare
 // FindMissingElement compares two slices of different length,
 // and returns the extra element between the two
 func FindMissingElement(s1, s2 []int) int {
-	s := append(s1,s2...)
+	s := append(s1, s2...)
 	missing := 0
-	for _,v := range s {
+	for _, v := range s {
 		missing = missing ^ v // Bitwise XOR operator between two same integers reverts 0
 	}
-	return missing	
-	}
+	return missing
+}
