071d9df8c91fbd92a662388071ea6c5bb2d3e1d7
Fanis Korlos <fanis.korlos@upstreamsystems.com>
2019-11-04 11:58:09 +0200

    Update solution using bitwise XOR operation

--- compare/missing.go
+++ compare/missing.go
@@ -4,17 +4,9 @@ package compare
 // and returns the extra element between the two
 func FindMissingElement(s1, s2 []int) int {
 	s := append(s1,s2...)
-	for len(s) > 1 {
-		// value := s[0]
-		for i, v := range s[:] {
-			if i == 0 {
-				continue
-			}
-			if s[0] == v { // Found duplicate
-				s[i], s[len(s)-1] = s[len(s)-1], s[i] // Put duplicate value in the end
-				s = s[:len(s)-1] // Re-slice to exlude the last element (duplicates))
-			}
-		}
+	missing := 0
+	for _,v := range s {
+		missing = missing ^ v // Bitwise XOR operator between two same integers reverts 0
 	}
-	return s[0]
+	return missing	
 	}
